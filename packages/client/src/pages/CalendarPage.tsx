f4e8dd0df7907b6368b77ed13f8daeb3372d4761
spocot <spencerkocot@gmail.com>
2026-06-18 19:45:10 -0400

    Move daily note indicator in calendar to top of indicator stack

--- packages/client/src/pages/CalendarPage.tsx
+++ packages/client/src/pages/CalendarPage.tsx
@@ -300,6 +300,17 @@ export default function CalendarPage() {
                 </span>
 
                 <div className="mt-1 flex flex-col gap-0.5">
+                  {hasDaily && (
+                    <span
+                      className="inline-block"
+                      title="Daily journal note"
+                    >
+                      <svg className="w-3 h-3 text-amber-600 dark:text-amber-400" fill="currentColor" viewBox="0 0 20 20">
+                        <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z" />
+                        <path fillRule="evenodd" d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z" clipRule="evenodd" />
+                      </svg>
+                    </span>
+                  )}
                   {hasNotes && (
                     <span
                       className={`inline-block w-2 h-2 rounded-full ${
@@ -317,17 +328,6 @@ export default function CalendarPage() {
                           : "activity"
                       }`}
                     />
-                  )}
-                  {hasDaily && (
-                    <span
-                      className="inline-block"
-                      title="Daily journal note"
-                    >
-                      <svg className="w-3 h-3 text-amber-600 dark:text-amber-400" fill="currentColor" viewBox="0 0 20 20">
-                        <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z" />
-                        <path fillRule="evenodd" d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z" clipRule="evenodd" />
-                      </svg>
-                    </span>
                   )}
                   {hasOutlook && (
                     <span
