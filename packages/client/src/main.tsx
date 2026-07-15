da7d813a2c0b36282fd4ed1568ab060f9c2473a1
spocot <spencerkocot@gmail.com>
2026-07-15 06:42:03 -0400

    Update BrowserRouter to pull basename from env var (passed to vite in github action)

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -6,10 +6,12 @@ import { store } from './store/redux/store';
 import App from './App';
 import './index.css';
 
+const basename = import.meta.env.BASE_URL.replace(/\/$/, '');
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename={basename}>
         <App />
       </BrowserRouter>
     </Provider>
